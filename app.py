62e0f3eca7e8da21dbb9d5305a3fef88adc95633
Darshan59063 <90100530+Darshan59063@users.noreply.github.com>
2023-07-01 01:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads/'
 
-# app.secret_key = "secret key"
+app.secret_key = "secret key"
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
